5976a36fe1c8503346235553ae4cfd9db65c00cc
Yevhen <onufrii.socnet@gmail.com>
2023-08-18 14:31:31 +0300

    feat: add new components: PrimaryButton, SvgSpriteIcon

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -5,12 +5,18 @@ import SvgSpriteIcon from './SvgSpriteIcon'
 interface PrimaryButtonProps {
   title: string
   svgSpriteId: string
+  href: string
 }
 
-const PrimaryButton: FC<PrimaryButtonProps> = ({ title, svgSpriteId }) => {
+const PrimaryButton: FC<PrimaryButtonProps> = ({ title, svgSpriteId, href }) => {
   return (
     <Box>
-      <Button variant="contained" endIcon={<SvgSpriteIcon svgSpriteId={svgSpriteId} />}>
+      <Button
+        href={href}
+        target="_blank"
+        variant="contained"
+        endIcon={<SvgSpriteIcon svgSpriteId={svgSpriteId} />}
+      >
         {title}
       </Button>
     </Box>
