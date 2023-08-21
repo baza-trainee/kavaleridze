8ff44652d222abd7c75609ada4f07599724a61be
Andrii <andriy30534>
2023-08-21 20:51:13 +0300

    added list mock list

---
+++ src/components/Footer/mock.ts
@@ -0,0 +1,22 @@
+export const listsItem = [
+  {
+    href: '/',
+    title: 'Головна',
+  },
+  {
+    href: '/',
+    title: 'Історія музею',
+  },
+  {
+    href: '/',
+    title: 'Події',
+  },
+  {
+    href: '/',
+    title: 'Іван Кавалерідзе',
+  },
+  {
+    href: '/',
+    title: 'Контакти',
+  },
+];
