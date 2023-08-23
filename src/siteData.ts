59c88fbab5eaf092bd8b027615e29b6c61527172
Yevhen <onufrii.socnet@gmail.com>
2023-08-23 14:32:10 +0300

    feat: add siteData file to store static site information

---
+++ src/siteData.ts
@@ -0,0 +1,60 @@
+const siteData = {
+  general: {
+    workingHours: 'Вт-Нд 11:00 – 18:00',
+    location: 'Андріївський узвіз, 21, Київ',
+    phone: '044 425-33-97',
+    email: 'kavaleridzemuseum@gmail.com',
+  },
+  buttonsLabels: {
+    tickets: 'Квитки',
+    support: 'Підтримати музей',
+  },
+  menuList: {
+    main: [
+      {
+        title: 'Головна',
+        href: '/',
+      },
+      {
+        title: 'Історія музею',
+        href: '/',
+      },
+      {
+        title: 'Події',
+        href: '/',
+      },
+      {
+        title: 'Іван Кавалерідзе',
+        href: '/',
+      },
+      {
+        title: 'Контакти',
+        href: '/',
+      },
+    ],
+    burgerSubMenu: [
+      {
+        title: 'Митець',
+        href: '/',
+      },
+      {
+        title: 'Біографія',
+        href: '/',
+      },
+      {
+        title: 'Кіно',
+        href: '/',
+      },
+      {
+        title: 'Скульптура',
+        href: '/',
+      },
+      {
+        title: 'Митець і Київ',
+        href: '/',
+      },
+    ],
+  },
+};
+
+export default siteData;
