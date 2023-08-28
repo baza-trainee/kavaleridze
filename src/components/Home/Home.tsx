713efe7a8ff3ab9e840893297c8f44cffe908cde
Andrii <andriy30534>
2023-08-28 23:17:01 +0300

    temporary connection form

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
+import Form from '../Form/Form';
 
 const Home: FC = () => {
-  return <div style={{ height: '200px', backgroundColor: 'white' }}></div>;
+  return (
+    <div style={{ height: '900px', backgroundColor: 'white' }}>
+      <Form />
+    </div>
+  );
 };
 
 export default Home;
