204c045a743b7870c259c13aa821c4c8732890b6
Andrii <andriy30534>
2023-08-29 09:40:44 +0300

    added TooltioInfo

--- src/components/Form/TooltipInfo.tsx
+++ src/components/Form/TooltipInfo.tsx
@@ -1,21 +1,21 @@
-import { IconButton, Tooltip, TooltipProps, tooltipClasses } from '@mui/material';
+import { IconButton, Tooltip, TooltipProps, Typography, tooltipClasses } from '@mui/material';
 import { Box, styled } from '@mui/system';
 import SvgSpriteIcon from '../PrimaryButton/SvgSpriteIcon';
 
 const MyTooltip = styled(({ className, ...props }: TooltipProps) => <Tooltip {...props} arrow classes={{ popper: className }} />)(
   ({ theme }) => ({
-    width: '150px',
-    [`& .${tooltipClasses.arrow}`]: {
-      color: theme.palette.common.black,
+    width: '250px',
+    opacity: 1,
+    [`& .${tooltipClasses.tooltip}`]: {
+      color: '#EEEEEE',
     },
-    [`& .${tooltipClasses.tooltip}`]: {},
   })
 );
 
 function TooltipInfo({ alert }: any) {
   return (
-    <Box sx={{ position: 'absolute', right: '8px', top: '26px' }}>
-      <MyTooltip enterTouchDelay={1} placement={'top-start'} title={alert}>
+    <Box sx={{ position: 'absolute', right: '8px', top: '30px' }}>
+      <MyTooltip enterTouchDelay={1} placement={'top-start'} title={<Typography variant="body1">{alert}</Typography>}>
         <IconButton sx={{ color: 'inherit' }}>
           <SvgSpriteIcon svgSpriteId="info_icon" />
         </IconButton>
