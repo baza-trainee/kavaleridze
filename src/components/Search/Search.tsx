c9549839d97f2114d44c8e3921d3bb878caacd90
Yevhen <onufrii.socnet@gmail.com>
2023-08-31 16:41:12 +0300

    feat: init commit

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,22 +1,52 @@
-import { Typography, Box, Container } from '@mui/material';
+import {} from '@emotion/react';
+import { Box, Container, InputAdornment, TextField, Typography, styled, useTheme } from '@mui/material';
 import { FC } from 'react';
-import { useSearchParams, Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import PrimaryButton from '../PrimaryButton/PrimaryButton';
+import SvgSpriteIcon from '../PrimaryButton/SvgSpriteIcon';
 import Section from '../Section/Section';
 
 const Search: FC = () => {
   const [searchParams] = useSearchParams();
   const search = searchParams.get('request');
+  const { palette } = useTheme();
+
+  const CustomTextField = styled(TextField)(({ theme, ...props }) => ({
+    '& .MuiInputBase-root': {
+      height: 38,
+      // width: '100%',
+      color: theme.palette.common.black,
+
+      '&:before': {
+        borderBottom: `1px solid ${theme.palette.text.secondary}`,
+      },
+    },
+  }));
 
   return (
     <Section variant="light">
       <Container>
-        <Box sx={{ px: 3, py: 3 }}>
+        <Typography component={'p'} sx={{ fontSize: '0.875rem', py: '16px', mb: '55px' }}>
+          there should be breadcrumbs
+        </Typography>
+        <Box component={'form'} sx={{ mb: '70px' }}>
+          <CustomTextField
+            fullWidth={true}
+            variant="standard"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SvgSpriteIcon sx={{ color: palette.text.secondary }} svgSpriteId="search_icon" />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        {/* <Box sx={{ px: 3, py: 3 }}>
           <Typography> Here is search results.</Typography>
           <Typography mb={4}>Search: {search?.split('-').join(' ')}</Typography>
-
           <PrimaryButton component={Link} href="/" title="Повернутись на головну" svgSpriteId="arrowLeft_icon" />
-        </Box>
+        </Box> */}
       </Container>
     </Section>
   );
