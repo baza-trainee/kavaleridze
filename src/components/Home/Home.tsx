56c3b8803d93b0d1df9edd8160850e71c58f1785
StanislavKuchma <kuchmastanislav73@gmail.com>
2023-09-06 13:51:28 +0300

    future/main_slider descktop
    init

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,23 +1,46 @@
 import { Box, Container, Typography } from '@mui/material';
 import { FC } from 'react';
 import Section from '../Section/Section';
+import MainSlider from '../MainSlider/MainSlider';
 
-const Home: FC = () => {
+const Home = () => {
   return (
     <>
       <Section variant="light">
-        <Container>
-          <Box sx={{ height: '200px' }}>
+     
+      
+         {/* <React.Fragment>
+      <CssBaseline /> */}
+      {/* <Container maxWidth="xl" sx={{ bgcolor: '#cfe8fc',height: 600, px: 0 }} > */}
+        <Typography variant="h5" sx={{ pl: 10,mb: 5}}>Вас вітає музей-майстерня Івана Кавалерідзе!</Typography>
+        {/* <Box sx={{ bgcolor: '#cfe8fc',height: 600 }} /> */}
+          <MainSlider/>'
+        {/* </Container> */}
+      
+        {/* </React.Fragment> */}
+        {/* //styleName: Desktop/Headline 1;
+font-family: Kyiv*Type Titling;
+font-size: 40px;
+font-weight: 500;
+line-height: 44px;
+letter-spacing: 0em;
+text-align: left; */}
+
+
+      
+                
+          {/* <Box sx={{ height: '200px' }}>
             <Typography variant="h2">Головна</Typography>
           </Box>
 
           <Box sx={{ height: '200px' }}>
             <Typography variant="h2">Перша секція</Typography>
-          </Box>
-          <Box sx={{ height: '200px' }}>
-            <Typography variant="h2">Друга секція</Typography>
-          </Box>
-          <Box sx={{ height: '200px' }}>
+        
+          </Box> */}
+          {/* <Box sx={{ height: '200px' }}> */}
+
+          {/* </Box> */}
+          {/* <Box sx={{ height: '200px' }}>
             <Typography variant="h2">Третя секція</Typography>
           </Box>
         </Container>
@@ -31,8 +54,8 @@ const Home: FC = () => {
       </Section>
       <Section variant="light">
         <Container>
-          <Box py={2}>
-            <Typography variant="h2">П'ята секція</Typography>
+          <Box py={2}> */}
+            {/* <Typography variant="h2">П'ята секція</Typography>
             <Typography variant="body1">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro temporibus eligendi maxime rem illum magnam quisquam quidem
               molestiae, cum quia, recusandae nulla possimus officiis laborum nisi, quas quos cumque alias repellat praesentium provident
@@ -41,10 +64,12 @@ const Home: FC = () => {
               eum earum at reprehenderit exercitationem assumenda mollitia explicabo blanditiis. Deserunt odit tempora a numquam laborum
               necessitatibus corrupti ipsa libero ex aliquam, obcaecati totam? Excepturi, nihil? Soluta minus odit ipsum laboriosam, quaerat
               voluptatem?s
-            </Typography>
-          </Box>
-        </Container>
-      </Section>
+          //   </Typography> */}
+  
+        {/* </Container>
+    
+           <MainSlider/> */}
+          </Section>
     </>
   );
 };
---
+++ src/components/MainSlider/MainSlider.tsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import { FC } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+// import React, { useRef, useState } from 'react';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+import 'swiper/scss/effect-coverflow'
+import slide_image_1 from "../../assets/images/img_1.png";
+// import slide_image_11 from "../../assets/images/img_11.png";
+import slide_image_2 from "../../assets/images/img_2.png";
+import slide_image_3 from "../../assets/images/img_3.png";
+import slide_image_4 from "../../assets/images/img_4.png";
+import slide_image_5 from "../../assets/images/img_5.png";
+// import slide_image_6 from "../../assets/images/img_6.png";
+// import slide_image_7 from "../../assets/images/img_7.png";
+import s from "./mainslider.module.css"
+
+const MainSlider = () => {
+  return (
+    <>
+      <div className={s.container}>
+
+
+
+      
+
+
+
+
+      <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          spaceBetween={600}
+          slidesPerView={'auto'}
+          loop={true}
+          coverflowEffect={{
+            rotate: 0,
+          
+            stretch: 0,
+            depth: 150,
+            modifier: 5,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className={s.mySwiper}
+        // grabCursor={true}
+        // slidesPerView={4}
+        // spaceBetween={50}
+        // effect={"coverflow"}
+        // centeredSlides={true}
+        // loop={true}
+        // slidesPerView={"auto"}
+        // coverflowEffect={{
+        //   rotate: 0,
+        //   stretch: 0,
+        //   depth: 150,
+        //   modifier: 2,
+        //   slideShadows: true,
+        // }}
+        // pagination={{ el: ".swiper-pagination", clickable: true }}
+        // navigation={{
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        //   clickable: true,
+        // }}
+        // modules={[EffectCoverflow, Pagination, Navigation]}
+        // className="swiper_container"
+      >
+        <SwiperSlide className={s.swiper_slide}>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide className={s.swiper_slide}>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide className={s.swiper_slide}>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide className={s.swiper_slide}>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide className={s.swiper_slide}>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          {/* <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div> */}
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+        
+</div>
+
+
+      
+
+
+       {/* <Swiper
+        grabCursor={true}
+        slidesPerView={"auto"}
+        spaceBetween={600}
+        effect={"coverflow"}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 150,
+          modifier: 5,
+          slideShadows: true,
+
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[
+          EffectCoverflow,
+          Pagination,
+          Navigation,
+
+        ]}
+        className={s.swiper}
+        >
+        <SwiperSlide className={s.swiper_slide}>
+            <img src={slide_image_1} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide className={s.swiper_slide}>
+            <img src={slide_image_2} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide className={s.swiper_slide}>
+            <img src={slide_image_3} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide className={s.swiper_slide}>
+            <img src={slide_image_4} alt="slide_image" />
+          </SwiperSlide  >
+          <SwiperSlide className={s.swiper_slide}>
+            <img src={slide_image_5} alt="slide_image" />
+          </SwiperSlide> */}
+
+          {/* <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div> */}
+        {/* </Swiper> */}
+    </>
+  );
+};
+
+export default MainSlider;
