b6b0b867ae132fafe467f96da1e7d7b025719b54
Anna Prutnik <prutnik.anna@gmail.com>
2023-09-06 11:49:07 +0300

    fix: remove scrollbar from cssBaseline

--- src/theme/themed-components/cssBaseline.modifier.ts
+++ src/theme/themed-components/cssBaseline.modifier.ts
@@ -1,8 +1,7 @@
 import { Components, Theme } from '@mui/material/styles';
-import darkScrollbar from '@mui/material/darkScrollbar';
 
 export const MuiCssBaseline: Components<Theme>['MuiCssBaseline'] = {
   styleOverrides: (theme) => ({
-    body: { ...darkScrollbar, backgroundColor: theme.palette.common.white, color: theme.palette.common.black },
+    body: { backgroundColor: theme.palette.common.white, color: theme.palette.common.black },
   }),
 };
