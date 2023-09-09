1fb6553a6547732791133c6889205e0e7ac22be6
Anna Prutnik <prutnik.anna@gmail.com>
2023-09-09 18:19:18 +0300

    fix: change touchStars on focus listener

--- src/components/Header/parts/SearchInput.tsx
+++ src/components/Header/parts/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, ChangeEvent, FormEvent, useRef, TouchEvent } from 'react';
+import { FC, useState, ChangeEvent, FormEvent, useRef, FocusEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, TextField, InputAdornment, styled, useTheme, useMediaQuery } from '@mui/material';
 import SvgSpriteIcon from '../../PrimaryButton/SvgSpriteIcon';
@@ -56,14 +56,17 @@ const SearchInput: FC<SearchInputProps> = ({ onCloseMenu }) => {
     }
   };
 
-  const onTouchStarts = (e: TouchEvent<HTMLDivElement>) => {
+  const onTouchStarts = (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     e.stopPropagation();
-    if (searchRef.current) {
-      searchRef.current.style.transform = 'TranslateY(-10000px)';
-      searchRef.current.focus();
-      setTimeout(function () {
-        if (searchRef.current) searchRef.current.style.transform = 'none';
-      }, 100);
+    const platform = navigator.platform.toLowerCase().includes('ios');
+    if (platform) {
+      if (searchRef.current) {
+        searchRef.current.style.transform = 'translateY(-100000px)';
+        searchRef.current.focus();
+        setTimeout(function () {
+          if (searchRef.current) searchRef.current.style.transform = 'none';
+        }, 100);
+      }
     }
   };
 
@@ -76,7 +79,7 @@ const SearchInput: FC<SearchInputProps> = ({ onCloseMenu }) => {
         placeholder="Пошук..."
         value={search}
         onChange={onChangeSearch}
-        onTouchStart={onTouchStarts}
+        onFocus={onTouchStarts}
         ref={searchRef}
         autoComplete="off"
         inputProps={{ maxLength: 120 }}
