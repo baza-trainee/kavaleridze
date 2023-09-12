0eb07df3738a41fb5f3750323b8f6a5fb6a53748
StanislavKuchma <kuchmastanislav73@gmail.com>
2023-09-12 20:07:41 +0300

    add new images

--- src/components/Home/MainSlider/MainSlider.tsx
+++ src/components/Home/MainSlider/MainSlider.tsx
@@ -6,38 +6,30 @@ import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/scrollbar';
 import 'swiper/scss/effect-coverflow'
-import slide_image_1 from "../../../assets/images/img_1.png";
-import slide_image_2 from "../../../assets/images/img_2.png";
-import slide_image_3 from "../../../assets/images/img_3.png";
-import slide_image_4 from "../../../assets/images/img_4.png";
-import slide_image_5 from "../../../assets/images/img_5.png";
-import slide_image_6 from "../../../assets/images/img_1_mobil.png";
-import slide_image_7 from "../../../assets/images/img_2_mobil.png";
-import slide_image_8 from "../../../assets/images/img_3_mobil.png";
-import slide_image_9 from "../../../assets/images/img_4_mobil.png";
-import slide_image_10 from "../../../assets/images/img_5_mobil.png";
-import slide_image_11 from "../../../assets/images/img_1_tablet.png";
-import slide_image_12 from "../../../assets/images/img_2_tablet.png";
-import slide_image_13 from "../../../assets/images/img_3_tablet.png";
-import slide_image_14 from "../../../assets/images/img_4_tablet.png";
-import slide_image_15 from "../../../assets/images/img_5_tablet.png";
+import slide_image_1 from "../../../assets/images_main_swiper/img_1.png";
+import slide_image_2 from "../../../assets/images_main_swiper/img_2.png";
+import slide_image_3 from "../../../assets/images_main_swiper/img_3.png";
+import slide_image_4 from "../../../assets/images_main_swiper/img_4.png";
+import slide_image_5 from "../../../assets/images_main_swiper/img_5.png";
+import slide_image_6 from "../../../assets/images_main_swiper/img_1_mobil.png";
+import slide_image_7 from "../../../assets/images_main_swiper/img_2_mobil.png";
+import slide_image_8 from "../../../assets/images_main_swiper/img_3_mobil.png";
+import slide_image_9 from "../../../assets/images_main_swiper/img_4_mobil.png";
+import slide_image_10 from "../../../assets/images_main_swiper/img_5_mobil.png";
+import slide_image_11 from "../../../assets/images_main_swiper/img_1_tablet.png";
+import slide_image_12 from "../../../assets/images_main_swiper/img_2_tablet.png";
+import slide_image_13 from "../../../assets/images_main_swiper/img_3_tablet.png";
+import slide_image_14 from "../../../assets/images_main_swiper/img_4_tablet.png";
+import slide_image_15 from "../../../assets/images_main_swiper/img_5_tablet.png";
 import { Typography,Box } from '@mui/material';
 import s from "./mainslider.module.css"
 
-// interface ProfileType {
-//   name: string
-//   image: string
-//   age: number | null
+// type MainSliderProps = {
+//    children: never[]
+//    media: string
+//    srcSet: string
+//    type: string
 // }
-
-// interface ProfilesProps {
-//   profiles: Array<ProfileType>
-// }
-
-// function Profiles(props: ProfilesProps) {
-
-// }
-
 
 
 const MainSlider:FC = () => {
@@ -66,11 +58,11 @@ const MainSlider:FC = () => {
           <SwiperSlide className={s.swiper_slide}>
             <picture>
               <source media="(max-width: 320px)"
-                srcset={slide_image_6}
+                srcSet={slide_image_6}
                 type="image/webp">
               </source>
               <source media="(max-width: 768px)"
-                srcset={slide_image_11}
+                srcSet={slide_image_11}
                 type="image/webp">
               </source>
               <img src={slide_image_1} alt="slide_image"  loading="lazy" />
@@ -80,11 +72,11 @@ const MainSlider:FC = () => {
           <SwiperSlide className={s.swiper_slide}>
             <picture>
               <source media="(max-width: 320px)"
-                srcset={slide_image_7}
+                srcSet={slide_image_7}
                 type="image/webp">
               </source>
               <source media="(max-width: 768px)"
-                srcset={slide_image_12}
+                srcSet={slide_image_12}
                 type="image/webp">
               </source>
               <img src={slide_image_2} alt="slide_image"  loading="lazy"/>
@@ -94,11 +86,11 @@ const MainSlider:FC = () => {
           <SwiperSlide className={s.swiper_slide}>
             <picture>
               <source media="(max-width: 320px)"
-                srcset={slide_image_8}
+                srcSet={slide_image_8}
                 type="image/webp">
               </source>
               <source media="(max-width: 768px)"
-                srcset={slide_image_13}
+                srcSet={slide_image_13}
                 type="image/webp">
               </source>
               <img src={slide_image_3} alt="slide_image"  loading="lazy" />
@@ -108,11 +100,11 @@ const MainSlider:FC = () => {
           <SwiperSlide className={s.swiper_slide}>
             <picture>
               <source media="(max-width: 320px)"
-                srcset={slide_image_9}
+                srcSet={slide_image_9}
                 type="image/webp">
               </source>
               <source media="(max-width: 768px)"
-                srcset={slide_image_14}
+               srcSet={slide_image_14}
                 type="image/webp">
               </source>
               <img src={slide_image_4} alt="slide_image"  loading="lazy"/>
@@ -122,11 +114,11 @@ const MainSlider:FC = () => {
           <SwiperSlide className={s.swiper_slide}>
            <picture>
               <source media="(max-width: 320px)"
-                srcset={slide_image_10}
+                srcSet={slide_image_10}
                 type="image/webp">
               </source>
               <source media="(max-width: 768px)"
-                srcset={slide_image_15}
+                srcSet={slide_image_15}
                 type="image/webp">
               </source>
               <img src={slide_image_5} alt="slide_image"  loading="lazy"/>
@@ -143,3 +135,5 @@ const MainSlider:FC = () => {
 };
 
 export default MainSlider;
+
+
