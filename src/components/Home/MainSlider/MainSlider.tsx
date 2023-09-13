204b9876f2676aa76399876d735835c4f5c0d348
StanislavKuchma <kuchmastanislav73@gmail.com>
2023-09-13 19:52:29 +0300

    test1

--- src/components/Home/MainSlider/MainSlider.tsx
+++ src/components/Home/MainSlider/MainSlider.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
 const MainSlider: FC = () => {
-  return <Container>MainSlider</Container>;
+  return <>
+    
+    <Container>
+      <Typography variant="h1" mb={{ xs: 3, md: 4, lg: 5 }} mx="auto" maxWidth={715} textAlign="center">
+       Вас вітає музей-майстерня Івана Кавалерідзе!
+      </Typography>
+    </Container>;
+  </> 
 };
 
 export default MainSlider;
