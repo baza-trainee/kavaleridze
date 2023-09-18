9cfebd6de56965424d7e05c052d6ca5f08c28bfc
StanislavKuchma <kuchmastanislav73@gmail.com>
2023-09-18 15:42:23 +0300

    fix style

--- src/components/Home/MainSlider/MainSlider.tsx
+++ src/components/Home/MainSlider/MainSlider.tsx
@@ -2,49 +2,30 @@ import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, EffectCoverflow } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow'
-import slide_image_1 from "../../../assets/tempData/images/mainSlider/img1.jpg"
-import slide_image_2 from "../../../assets/tempData/images/mainSlider/img2.jpg" 
-import slide_image_3 from "../../../assets/tempData/images/mainSlider/img3.jpg" 
-import slide_image_4 from "../../../assets/tempData/images/mainSlider/img4.jpg" 
-import slide_image_5 from "../../../assets/tempData/images/mainSlider/img5.jpg" 
-import slide_image_11 from  "../../../assets/tempData/images/mainSlider/img1_tablet.jpg"
-import slide_image_12 from  "../../../assets/tempData/images/mainSlider/img2_tablet.jpg"
-import slide_image_13 from  "../../../assets/tempData/images/mainSlider/img3_tablet.jpg"
-import slide_image_14 from  "../../../assets/tempData/images/mainSlider/img4_tablet.jpg"
-import slide_image_15 from "../../../assets/tempData/images/mainSlider/img5_tablet.jpg"
-import slide_image_6 from "../../../assets/tempData/images/mainSlider/img1_mobil.jpg"
-import slide_image_7 from "../../../assets/tempData/images/mainSlider/img2_mobil.jpg"
-import slide_image_8 from "../../../assets/tempData/images/mainSlider/img3_mobil.jpg"
-import slide_image_9 from "../../../assets/tempData/images/mainSlider/img4_mobil.jpg"
-import slide_image_10 from "../../../assets/tempData/images/mainSlider/img5_mobil.jpg"
-
-// import Section from '../../Section/Section';
-// import s from "./mainslider.css"
-// import { PropsWithChildren } from 'react'
-
-// interface Slide {
-//   picture: any
-//   source: any
-//   img: any
-//   className: string
-// }
-// type PropsWithChildren<P> = P & { children?: ReactNode };
-// type FooProps = {
-//   picture: any
-//   source: any
-//   img: any
-//   // look here 👇
-//   children: ReactNode
-// }
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "./mainslider.css";
+import slide_image_1 from "../../../assets/tempData/images/mainSlider/img1.jpg";
+import slide_image_2 from "../../../assets/tempData/images/mainSlider/img2.jpg" ;
+import slide_image_3 from "../../../assets/tempData/images/mainSlider/img3.jpg" ;
+import slide_image_4 from "../../../assets/tempData/images/mainSlider/img4.jpg" ;
+import slide_image_5 from "../../../assets/tempData/images/mainSlider/img5.jpg" ;
+import slide_image_11 from  "../../../assets/tempData/images/mainSlider/img1_tablet.jpg";
+import slide_image_12 from  "../../../assets/tempData/images/mainSlider/img2_tablet.jpg";
+import slide_image_13 from  "../../../assets/tempData/images/mainSlider/img3_tablet.jpg";
+import slide_image_14 from  "../../../assets/tempData/images/mainSlider/img4_tablet.jpg";
+import slide_image_15 from "../../../assets/tempData/images/mainSlider/img5_tablet.jpg";
+import slide_image_6 from "../../../assets/tempData/images/mainSlider/img1_mobil.jpg";
+import slide_image_7 from "../../../assets/tempData/images/mainSlider/img2_mobil.jpg";
+import slide_image_8 from "../../../assets/tempData/images/mainSlider/img3_mobil.jpg";
+import slide_image_9 from "../../../assets/tempData/images/mainSlider/img4_mobil.jpg";
+import slide_image_10 from "../../../assets/tempData/images/mainSlider/img5_mobil.jpg";
 
 
 const MainSlider:FC = () => {
   return (
     <>
-      {/* <Section variant="light"> */}
-      <Box className="container">
+       <Box className="container">
       <Typography variant="h2" mb={{ xs: 3, md: 4, lg: 5 }} sx={{ ml: 10}} mt={{ xs: 5, md: 10, lg: 15 }} maxWidth={1000} >
            Вас вітає музей-майстерня Івана Кавалерідзе!
       </Typography>
@@ -132,7 +113,6 @@ const MainSlider:FC = () => {
           </SwiperSlide>
        </Swiper>
         </Box>
-        {/* </Section> */}
     </>
   );
 };
