d9fcccdd7e8609d414ffdb21bebf1655fe8947b4
Yevhen <onufrii.socnet@gmail.com>
2023-09-27 12:39:44 +0300

    fix: AdminTextField styles

--- src/components/admin/Common/AdminTextField.tsx
+++ src/components/admin/Common/AdminTextField.tsx
@@ -1,21 +1,13 @@
-import { InputBase, InputBaseProps, styled } from '@mui/material';
+import { TextField, TextFieldProps, styled } from '@mui/material';
 
-const AdminTextField = styled(InputBase)<InputBaseProps>(({ theme, error }) => ({
-  borderRadius: '8px',
-  color: theme.palette.common.black,
-  minWidth: '358px',
-  padding: '12px',
-  border: error ? `1px solid ${theme.palette.error.main}` : `1px solid ${theme.palette.common.black}`,
-
-  ':hover': {
-    borderColor: error ? theme.palette.error.main : theme.palette.primary.main,
+const AdminTextField = styled(TextField)<TextFieldProps>(() => ({
+  fieldset: {
+    borderRadius: '8px',
   },
-  '.MuiInputBase-input': {
+  '.MuiOutlinedInput-root': {
+    height: '52px',
+    padding: '12px 16px',
     lineHeight: 1.5,
-    height: '27px',
-  },
-  '&.Mui-focused': {
-    borderColor: error ? theme.palette.error.main : theme.palette.common.black,
   },
 }));
 
