2f7b20ac9a8a61e2bedb266613864d05d0e103c7
Volodymyr Zhukivskyi <volodimirzukivskij@gmail.com>
2023-10-02 19:57:19 +0300

    fix: size button

--- src/components/main/Stub/Stub.tsx
+++ src/components/main/Stub/Stub.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Button } from '@mui/material';
 import Section from '../Common/Section';
-import { Wrapper } from './styles';
+import { ButtonStub, Wrapper } from './styles';
 
 const Stub: FC = () => {
   return (
@@ -12,9 +12,9 @@ const Stub: FC = () => {
           Вибачте, на даний момент ця сторінка знаходиться в розробці
         </Typography>
 
-        <Button variant="secondary" component={Link} to="/" style={{ marginTop: '20px' }}>
+        <ButtonStub  variant="primary"  component={Link} to="/" style={{ marginTop: '20px' }}>
           На головну сторінку
-        </Button>
+        </ButtonStub>
       </Wrapper>
     </Section>
   );
