d57d9ccece95d69929e67608fcd15b6bcbf3d272
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-10-16 21:38:58 +0300

    feat: add loader spinner with timeout

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -8,10 +8,22 @@ import Banner from './Banner';
 import { WrapperImg } from './styles';
 import { truncateDescription } from '@/helpers/truncateString';
 
+import CircularProgress from '@mui/material/CircularProgress';
+import { RotatingLines, ThreeDots } from 'react-loader-spinner';
+
 const Events: FC = () => {
   const [cardsEvent, setItems] = useState(dataInfo);
   const [visibleItems, setVisibleItems] = useState(3);
   const theme = useTheme();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     setItems(dataInfo);
@@ -23,61 +35,73 @@ const Events: FC = () => {
 
   return (
     <Section variant="light">
-      <Banner />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: { xs: '32px', lg: '40px' },
-          marginTop: { xs: '32px', md: '44px' },
-          paddingBottom: { xs: '40px', md: '32px' },
-        }}>
-        {cardsEvent.slice(0, visibleItems).map((item, index) => (
-          <Container key={index} sx={{ borderBottom: `1px solid ${theme.palette.gray.main} ` }}>
-            <Box key={index} sx={{ padding: { xs: '24px 0' } }}>
-              <Box
-                sx={{
-                  display: 'grid',
-                  gridTemplateColumns: { md: '1fr 1fr', lg: '494px 436px' },
-                  gap: { xs: '16px', md: '24px', lg: '48px' },
-                }}>
-                <WrapperImg>
-                  <img src={item.img} alt="" />
-                </WrapperImg>
-                <Box>
+      {loading && (
+        <Box sx={{ display: 'grid', placeItems: 'center', height: '50vh' }}>
+          <CircularProgress size="4rem" />
+          <RotatingLines strokeColor="#F0B92D" strokeWidth="5" animationDuration="0.75" width="96" visible={true} />
+          <ThreeDots height="80" width="80" radius="9" color="#F0B92D" ariaLabel="three-dots-loading" wrapperStyle={{}} visible={true} />
+        </Box>
+      )}
+
+      {!loading && (
+        <>
+          <Banner />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: { xs: '32px', lg: '40px' },
+              marginTop: { xs: '32px', md: '44px' },
+              paddingBottom: { xs: '40px', md: '32px' },
+            }}>
+            {cardsEvent.slice(0, visibleItems).map((item, index) => (
+              <Container key={index} sx={{ borderBottom: `1px solid ${theme.palette.gray.main} ` }}>
+                <Box key={index} sx={{ padding: { xs: '24px 0' } }}>
                   <Box
                     sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      gap: '16px',
+                      display: 'grid',
+                      gridTemplateColumns: { md: '1fr 1fr', lg: '494px 436px' },
+                      gap: { xs: '16px', md: '24px', lg: '48px' },
                     }}>
-                    <Typography variant="h2">{truncateDescription(item.cardTitle, 100)}</Typography>
-                    <Typography variant="body1" sx={{ fontWeight: '600' }}>
-                      {item.dataPerformance}
-                    </Typography>
-                    <Typography variant="caption">{truncateDescription(item.description, 150)}</Typography>
+                    <WrapperImg>
+                      <img src={item.img} alt="" />
+                    </WrapperImg>
+                    <Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          gap: '16px',
+                        }}>
+                        <Typography variant="h2">{truncateDescription(item.cardTitle, 100)}</Typography>
+                        <Typography variant="body1" sx={{ fontWeight: '600' }}>
+                          {item.dataPerformance}
+                        </Typography>
+                        <Typography variant="caption">{truncateDescription(item.description, 150)}</Typography>
+                      </Box>
+                      <ButtonWithIcon
+                        variant="tertiary"
+                        component={RouterLink}
+                        sx={{ marginTop: '24px' }}
+                        to={dataInfo[index].cardTitle}
+                        svgSpriteId="breadcrumbsSeparator_icon"
+                        title="Читати далі"
+                      />
+                    </Box>
                   </Box>
-                  <ButtonWithIcon
-                    variant="tertiary"
-                    component={RouterLink}
-                    sx={{ marginTop: '24px' }}
-                    to={dataInfo[index].cardTitle}
-                    svgSpriteId="breadcrumbsSeparator_icon"
-                    title="Читати далі"
-                  />
                 </Box>
-              </Box>
-            </Box>
-          </Container>
-        ))}
-      </Box>
-      <Box sx={{ width: '100%', textAlign: 'center', marginBottom: { xs: '60px', md: '80px' } }}>
-        {visibleItems < cardsEvent.length && (
-          <Button sx={{ width: '248px' }} onClick={handlerLoadMore} variant="secondary">
-            Показати більше
-          </Button>
-        )}
-      </Box>
+              </Container>
+            ))}
+          </Box>
+          <Box sx={{ width: '100%', textAlign: 'center', marginBottom: { xs: '60px', md: '80px' } }}>
+            {visibleItems < cardsEvent.length && (
+              <Button sx={{ width: '248px' }} onClick={handlerLoadMore} variant="secondary">
+                Показати більше
+              </Button>
+            )}
+          </Box>
+        </>
+      )}
     </Section>
   );
 };
